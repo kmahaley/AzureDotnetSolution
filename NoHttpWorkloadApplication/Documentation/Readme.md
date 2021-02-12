8c7eb5f570f82ad06aaa4b725f7d20f1e04fb837
Kartik Mahaley <kamahale@microsoft.com>
2021-02-12 11:58:07 -0500

    Added non web application to demo hosted services concept

---
+++ NoHttpWorkloadApplication/Documentation/Readme.md
@@ -0,0 +1,35 @@
+﻿# NoHttpWorkloadApplication
+
+## Build
+
+  - `git clone https://github.com/kmahaley/AzureDotnetSolution.git`
+  - cd into AzureDotnetSolution -> NoHttpWorkloadApplication
+  - run following commands
+    - `dotnet build`
+    - `dotnet run`
+    - Verify terminal logs
+
+## Fundamentals
+
+  - Http workload:
+```
+public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });  
+```
+
+  - Non http workload:
+```
+public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+               services.AddHostedService<Worker>();
+            }); 
+```
+
+## References
+  - https://docs.microsoft.com/en-us/aspnet/core/fundamentals/?view=aspnetcore-5.0&tabs=windows
---
+++ NoHttpWorkloadApplication/Services/BookService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NoHttpWorkloadApplication.Services
+{
+    public class BookService : IHostedService
+    {
+        private readonly ILogger logger;
+
+        public BookService(ILogger<BookService> logger,
+            IHostApplicationLifetime appLifetime)
+        {
+            this.logger = logger;
+
+            appLifetime.ApplicationStarted.Register(OnStarted);
+            appLifetime.ApplicationStopping.Register(OnStopping);
+            appLifetime.ApplicationStopped.Register(OnStopped);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("1. StartAsync has been called.");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("4. StopAsync has been called.");
+
+            return Task.CompletedTask;
+        }
+
+        private void OnStarted()
+        {
+            logger.LogInformation("2. OnStarted has been called.");
+        }
+
+        private void OnStopping()
+        {
+            logger.LogInformation("3. OnStopping has been called.");
+        }
+
+        private void OnStopped()
+        {
+            logger.LogInformation("5. OnStopped has been called.");
+        }
+    }
+}
