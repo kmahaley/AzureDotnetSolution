ea48401cc4adc549ce7f8104e90b6dd4b4443f4d
Kartik Mahaley <kamahale@microsoft.com>
2021-10-27 14:25:29 -0400

    Added object constraints

--- SimulateDownStreamApplication/Model/Student.cs
+++ SimulateDownStreamApplication/Model/Student.cs
@@ -1,9 +1,12 @@
-﻿namespace SimulateDownStreamApplication.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimulateDownStreamApplication.Model
 {
     public class Student
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
 
         public int RollNumber { get; set; }
