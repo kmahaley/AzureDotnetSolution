187a4dd9928c76fc75a3b9e43e7c4e932718a40e
Kartik Mahaley <kamahale@microsoft.com>
2021-12-31 20:20:20 -0500

    fix unit test

--- UnitTests/CoreWebApplication.UnitTests/ItemControllerTests.cs
+++ UnitTests/CoreWebApplication.UnitTests/ItemControllerTests.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Logging.Console;
 using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreWebApplication.UnitTests
 {
@@ -33,16 +34,28 @@ namespace CoreWebApplication.UnitTests
         [TestInitialize]
         public void Setup()
         {           
-            //listOfRepositoriesMock
-            //    .Setup(repoList => repoList.FirstOrDefault(It.IsAny<Func<IRepository, bool>>()))
-            //    .Returns(repositoryMock.Object);
-
-            //controller = new ItemController(listOfRepositoriesMock.Object, loggerMock.Object);
             controller = new ItemController(repositoryMock.Object, loggerMock.Object);
         }
 
         [TestMethod]
         public async Task GetItemAsync_WithNonExistingItem_ReturnsNotFoundAsync()
+        {
+            //Arrage
+            var id = Guid.NewGuid();
+            repositoryMock
+                .Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(() => null);
+
+            //Act
+            var actionResult  = await controller.GetItemAsync(Guid.NewGuid());
+
+            //Assert
+            var actualNotFound = actionResult.Result as NotFoundResult;
+            Assert.AreEqual(404, actualNotFound.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task GetItemAsync_WithExistingItem_ReturnsItem()
         {
             //Arrage
             var id = Guid.NewGuid();
@@ -59,10 +72,47 @@ namespace CoreWebApplication.UnitTests
                 .ReturnsAsync(() => item);
 
             //Act
-            var result = await controller.GetItemAsync(Guid.NewGuid());
-            var actualItem = result.Value;
+            var actionResult  = await controller.GetItemAsync(Guid.NewGuid());
+
             //Assert
+            var actualOkItem = actionResult.Result as OkObjectResult;
+            var actualItem = (Item)actualOkItem.Value;
             Assert.AreEqual(item.Name, actualItem.Name);
+
+        }
+
+        [TestMethod]
+        public async Task GetItemsAsync_ReturnsListOf2Items()
+        {
+            //Arrage
+            var id1 = Guid.NewGuid();
+            var item1 = new Item
+            {
+                Id = id1,
+                Name = "apple",
+                Price = 2344,
+                CreatedDate = DateTimeOffset.UtcNow,
+            };
+            var id2 = Guid.NewGuid();
+            var item2 = new Item
+            {
+                Id = id2,
+                Name = "apple",
+                Price = 2344,
+                CreatedDate = DateTimeOffset.UtcNow,
+            };
+            var expectedList = new List<Item>() { item1, item2 };
+            repositoryMock
+                .Setup(repo => repo.GetItemsAsync())
+                .ReturnsAsync(() => expectedList);
+
+            //Act
+            var actionResult  = await controller.GetItemsAsync();
+
+            //Assert
+            var actualOkItem = actionResult.Result as OkObjectResult;
+            var actualItems = (List<Item>)actualOkItem.Value;
+            Assert.AreEqual(expectedList, actualItems);
 
         }
     }
